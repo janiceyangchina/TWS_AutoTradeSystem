11a8712e5422110b6a5e3d75095ab28f3e801c3a
jyang <janiceyangchina@gmail.com>
2017-12-12 20:50:21 -0500

    - Add StrategyUtil file to process strategy related method

---
+++ src/apidemo/util/StrategyUtil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apidemo.util;
+
+import com.ib.client.Contract;
+
+/**
+ *
+ * @author jyang
+ */
+public class StrategyUtil {
+    
+    // duration 3 , duration unit days bar_size 1 day
+    public static boolean averageStockPrice(Contract contract){
+      return false;
+    }
+    
+    public static void storeOrderHistory(String timeStamp){
+       // check the time order created stock symbols, price, quantity, 
+       // total amount 
+       return;
+    }
+}
